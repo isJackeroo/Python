1e4c7206ca28ced82c283f26cffe833f05e10339
Jackeroo <62932009+SuperJackeroo@users.noreply.github.com>
2021-09-25 08:56:51 +0800

    Update simply and complex password generator

--- Random_Password_Generator.py
+++ Random_Password_Generator.py
@@ -1,10 +1,19 @@
-import random
-low="abcdefghijklmnopqrstuvwxyz"
-upp="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-num="0123456789"
-sym="!@#$%^&*"
+'''
+#简单生成全英文大小写随机密码：
+import string, random
+length = int(input("The Password Length You Want to Set:"))
+for i in range(1):
+    RandomPassword = ''.join(random.sample(string.ascii_letters,length))
+    print(RandomPassword)
 
-all=low+upp+num+sym
-length=10   #修改密码长度
-password="".join(random.sample(all,length))
+'''
+
+#生成包含大小写字母、英文标点符号的自定义长度随机密码
+import string, random
+ascii_words = string.ascii_letters
+digital_num = string.digits
+english_pun = string.punctuation
+all = ascii_words + digital_num + english_pun
+length = int(input("The Password Length You Want to Set:"))
+password = ''.join(random.sample(all,length))
 print(password)
